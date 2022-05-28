53f400832de0bbb4709c3c4d198e088f6eb4a90c
ThePojers <61025548+ThePojers@users.noreply.github.com>
2022-05-28 22:07:12 +0200

    udpate

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,29 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+
+  pages: {
+    popup: {
+      template: 'public/browser-extension.html',
+      entry: './src/popup/main.js',
+      title: 'Popup'
+    },
+  },
+
+  pluginOptions: {
+    browserExtension: {
+      componentOptions: {
+        background: {
+          entry: 'src/background.js'
+        },
+        contentScripts: {
+          entries: {
+            'content-script': [
+              'src/content-scripts/content-script.js'
+            ]
+          }
+        }
+      }
+    }
+  }
 })
